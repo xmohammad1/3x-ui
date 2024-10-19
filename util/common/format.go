4f1c306613eca43f92c044bc0cedcbe4c097d3ec
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2024-10-20 00:01:03 +0330

    Update format.go

--- util/common/format.go
+++ util/common/format.go
@@ -5,17 +5,17 @@ import (
 )
 
 func FormatTraffic(trafficBytes int64) string {
-  units := []string{"B", "KB", "MB", "GB", "TB", "PB"}
-  unitIndex := 0
-  size := float64(trafficBytes)
+	units := []string{"B", "KB", "MB", "GB", "TB", "PB"}
+	unitIndex := 0
+	size := float64(trafficBytes)
 
-  for size >= 1024 && unitIndex < len(units)-1 {
-    if unitIndex == 4 {
-       size /= 1000
-    } else {
-       size /= 1024
-    }
-    unitIndex++
-  }
-  return fmt.Sprintf("%.2f%s", size, units[unitIndex])
+	for size >= 1024 && unitIndex < len(units)-1 {
+		if unitIndex < 3 {
+			size /= 1024
+		} else {
+			size /= 1000
+		}
+		unitIndex++
+	}
+	return fmt.Sprintf("%.2f%s", size, units[unitIndex])
 }
